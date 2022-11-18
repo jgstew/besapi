6363638489ccd562036f04adcc0cbd51392e7fb6
jgstew <james@jgstew.com>
2022-11-18 11:58:45 -0500

    working example stop_open_completed_actions

---
+++ examples/stop_open_completed_actions.py
@@ -0,0 +1,24 @@
+import besapi
+
+SESSION_RELEVANCE = """ids of bes actions whose( (targeted by list flag of it OR targeted by id flag of it) AND not reapply flag of it AND not group member flag of it AND "Open"=state of it AND (now - time issued of it) >= 8 * day )"""
+
+
+def main():
+    """Execution starts here"""
+    print("main()")
+    bes_conn = besapi.besapi.get_bes_conn_using_config_file()
+    bes_conn.login()
+
+    session_result = bes_conn.session_relevance_array(SESSION_RELEVANCE)
+
+    # print(session_result)
+
+    # https://developer.bigfix.com/rest-api/api/action.html
+    for action_id in session_result:
+        print("Stopping Action:", action_id)
+        action_stop_result = bes_conn.post("action/" + action_id + "/stop", "")
+        print(action_stop_result)
+
+
+if __name__ == "__main__":
+    main()
