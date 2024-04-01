47cc21e131e2fa33f95e5caf90fa55f412d22b1a
jgstew <james@jgstew.com>
2024-04-01 13:59:37 -0400

    attempt to release a new version again

--- src/besapi/besapi.py
+++ src/besapi/besapi.py
@@ -29,7 +29,7 @@ import requests
 from lxml import etree, objectify
 from pkg_resources import resource_filename
 
-__version__ = "3.2.5"
+__version__ = "3.2.6"
 
 besapi_logger = logging.getLogger("besapi")
 
