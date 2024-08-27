04fec57cc86f6244a9a97568b526fcce26c42d80
JGStew <james@jgstew.com>
2024-08-27 14:03:56 -0400

    test publishing fix

--- src/besapi/besapi.py
+++ src/besapi/besapi.py
@@ -29,7 +29,7 @@ import requests
 from lxml import etree, objectify
 from pkg_resources import resource_filename
 
-__version__ = "3.3.1"
+__version__ = "3.3.2"
 
 besapi_logger = logging.getLogger("besapi")
 
