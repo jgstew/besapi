31a6afff8d8c40e387a15683f0576646c5affb74
JGStew <james@jgstew.com>
2024-08-29 21:23:03 -0400

    update version to reflect new minimum python version

--- src/besapi/besapi.py
+++ src/besapi/besapi.py
@@ -29,7 +29,7 @@ import requests
 from lxml import etree, objectify
 from pkg_resources import resource_filename
 
-__version__ = "3.5.1"
+__version__ = "3.7.1"
 
 besapi_logger = logging.getLogger("besapi")
 
