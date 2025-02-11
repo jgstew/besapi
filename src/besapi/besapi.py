1e226f093af1048a9f13da251b19037f7679c5c1
JGStew <james@jgstew.com>
2025-02-11 17:37:47 -0500

    minor update

--- src/besapi/besapi.py
+++ src/besapi/besapi.py
@@ -29,7 +29,7 @@ import requests
 from lxml import etree, objectify
 from pkg_resources import resource_filename
 
-__version__ = "3.7.6"
+__version__ = "3.7.7"
 
 besapi_logger = logging.getLogger("besapi")
 
