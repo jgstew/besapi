84c0a642a68017d650a94cafa01d960163b8227b
JGStew <james@jgstew.com>
2025-02-25 12:41:46 -0500

    refactor action_and_monitor into function

--- examples/action_and_monitor.py
+++ examples/action_and_monitor.py
@@ -1,7 +1,10 @@
 """
-Create an action from fixlet/task xml bes file and monitor it's results for ~300 seconds
+Create an action from a fixlet or task xml bes file
+and monitor it's results for ~300 seconds
 
 requires `besapi`, install with command `pip install besapi`
+
+NOTE: this script requires besapi v3.3.3+ due to use of besapi.plugin_utilities
 """
 
 import logging
@@ -64,7 +67,7 @@ def get_invoke_file_name(verbose=0):
 def get_action_combined_relevance(relevances):
     """take array of ordered relevance clauses and return relevance string for action"""
 
-    relevance = ""
+    relevance_combined = ""
 
     if not relevances:
         return "False"
@@ -74,64 +77,24 @@ def get_action_combined_relevance(relevances):
         return relevances[0]
     if len(relevances) > 0:
         for clause in relevances:
-            if len(relevance) == 0:
-                relevance = clause
+            if len(relevance_combined) == 0:
+                relevance_combined = clause
             else:
-                relevance = "( " + relevance + " ) AND ( " + clause + " )"
+                relevance_combined = (
+                    "( " + relevance_combined + " ) AND ( " + clause + " )"
+                )
 
-    return relevance
+    return relevance_combined
 
 
-def main():
-    """Execution starts here"""
-    print("main()")
-
-    print("NOTE: this script requires besapi v3.3.3+ due to besapi.plugin_utilities")
-
-    parser = besapi.plugin_utilities.setup_plugin_argparse()
-
-    # add additonal arg specific to this script:
-    parser.add_argument(
-        "-f",
-        "--file",
-        help="xml bes file to create an action from",
-        required=False,
-        type=str,
-    )
-    # allow unknown args to be parsed instead of throwing an error:
-    args, _unknown = parser.parse_known_args()
-
-    # allow set global scoped vars
-    global bes_conn, verbose, invoke_folder
-    verbose = args.verbose
-
-    # get folder the script was invoked from:
-    invoke_folder = get_invoke_folder()
-
-    log_file_path = os.path.join(
-        get_invoke_folder(verbose), get_invoke_file_name(verbose) + ".log"
-    )
-
-    print(log_file_path)
-
-    logging_config = besapi.plugin_utilities.get_plugin_logging_config(
-        log_file_path, verbose, args.console
-    )
-
-    logging.basicConfig(**logging_config)
-
-    logging.info("---------- Starting New Session -----------")
-    logging.debug("invoke folder: %s", invoke_folder)
-    logging.debug("Python version: %s", platform.sys.version)
-    logging.debug("BESAPI Module version: %s", besapi.besapi.__version__)
-    logging.debug("this plugin's version: %s", __version__)
-
-    bes_conn = besapi.plugin_utilities.get_besapi_connection(args)
-
+def action_and_monitor(bes_conn, file_path):
+    """Take action from bes xml file
+    monitor results of action"""
+    # TODO: allow input variable for custom targeting
     # default to empty string:
     custom_relevance_xml = ""
 
-    tree = lxml.etree.parse(args.file)
+    tree = lxml.etree.parse(file_path)
 
     # //BES/*[self::Task or self::Fixlet]/*[@id='elid']/name()
     bes_type = str(tree.xpath("//BES/*[self::Task or self::Fixlet]")[0].tag)
@@ -237,6 +200,57 @@ def main():
                 break
     except KeyboardInterrupt:
         print("\nloop interuppted")
+
+    return previous_result
+
+
+def main():
+    """Execution starts here"""
+    print("main()")
+
+    print("NOTE: this script requires besapi v3.3.3+ due to besapi.plugin_utilities")
+
+    parser = besapi.plugin_utilities.setup_plugin_argparse()
+
+    # add additonal arg specific to this script:
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="xml bes file to create an action from",
+        required=False,
+        type=str,
+    )
+    # allow unknown args to be parsed instead of throwing an error:
+    args, _unknown = parser.parse_known_args()
+
+    # allow set global scoped vars
+    global bes_conn, verbose, invoke_folder
+    verbose = args.verbose
+
+    # get folder the script was invoked from:
+    invoke_folder = get_invoke_folder()
+
+    log_file_path = os.path.join(
+        get_invoke_folder(verbose), get_invoke_file_name(verbose) + ".log"
+    )
+
+    print(log_file_path)
+
+    logging_config = besapi.plugin_utilities.get_plugin_logging_config(
+        log_file_path, verbose, args.console
+    )
+
+    logging.basicConfig(**logging_config)
+
+    logging.info("---------- Starting New Session -----------")
+    logging.debug("invoke folder: %s", invoke_folder)
+    logging.debug("Python version: %s", platform.sys.version)
+    logging.debug("BESAPI Module version: %s", besapi.besapi.__version__)
+    logging.debug("this plugin's version: %s", __version__)
+
+    bes_conn = besapi.plugin_utilities.get_besapi_connection(args)
+
+    action_and_monitor(bes_conn, args.file)
 
     logging.info("---------- END -----------")
 
