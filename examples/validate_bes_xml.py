79af124b36d271ee022abb3806cbf98ca7382436
JGStew <james@jgstew.com>
2025-03-03 18:21:29 -0500

    tweak validate bes

--- examples/validate_bes_xml.py
+++ examples/validate_bes_xml.py
@@ -7,14 +7,18 @@ requires `besapi`, install with command `pip install besapi`
 import besapi
 
 
+def validate_xml_bes_file(file_path):
+    with open(file_path, "rb") as file:
+        file_data = file.read()
+
+    return besapi.besapi.validate_xsd(file_data)
+
+
 def main(file_path):
     """Execution starts here"""
     print("main()")
 
-    with open(file_path, "rb") as file:
-        file_data = file.read()
-
-    print(besapi.besapi.validate_xsd(file_data))
+    print(validate_xml_bes_file(file_path))
 
 
 if __name__ == "__main__":
