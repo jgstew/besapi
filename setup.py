d97157c77dcc8c9325517273f9393998833cf0f9
JGStew <james@jgstew.com>
2025-03-12 10:15:49 -0400

    migrate to setup.cfg from setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python
 """File to configure python build and packaging for pip."""
 
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name="besapi",
-    # version= moved to setup.cfg
-    author="Matt Hansen, James Stewart",
-    author_email="hansen.m@psu.edu, james@jgstew.com",
-    description="Library for working with the BigFix REST API",
-    license="MIT",
-    keywords="bigfix iem tem rest api",
-    url="https://github.com/jgstew/besapi",
-    # long_description= moved to setup.cfg
-    packages=["besapi", "bescli"],
-    package_data={"besapi": ["schemas/*.xsd"]},
-    install_requires=["requests", "lxml", "cmd2", "setuptools"],
-    include_package_data=True,
-    package_dir={"": "src"},
-)
+if __name__ == "__main__":
+    setup()
