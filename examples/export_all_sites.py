c1d03d51cce314958cf91e17c0c805f54747eb08
JGStew <james@jgstew.com>
2025-03-26 15:17:21 -0400

    add comments

--- examples/export_all_sites.py
+++ examples/export_all_sites.py
@@ -15,13 +15,16 @@ def main():
     bes_conn = besapi.besapi.get_bes_conn_using_config_file()
     bes_conn.login()
 
+    # Create export folder if it doesn't exist
     try:
         os.mkdir("export")
     except FileExistsError:
         pass
 
+    # Change working directory to export folder
     os.chdir("export")
 
+    # Export all sites
     bes_conn.export_all_sites()
 
 
