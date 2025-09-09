e089cdd63f70e00a84c6c93d32d3e045b3aa1d51
JGStew <james@jgstew.com>
2025-09-09 11:53:37 -0400

    make this able to run as a plugin

--- examples/fixlet_add_mime_field.py
+++ examples/fixlet_add_mime_field.py
@@ -5,17 +5,69 @@ Use this session relevance to find fixlets missing the mime field:
 - https://bigfix.me/relevance/details/3023816
 """
 
+import logging
+import ntpath
+import os
+import platform
+import sys
 import urllib.parse
 
 import lxml.etree
 
 import besapi
+import besapi.plugin_utilities
 
 MIME_FIELD_NAME = "x-relevance-evaluation-period"
 MIME_FIELD_VALUE = "01:00:00"  # 1 hour
 
 # Must return fixlet or task objects:
 session_relevance_multiple_fixlets = """custom bes fixlets whose(exists (it as lowercase) whose(it contains " wmi" OR it contains " descendant") of relevance of it AND not exists mime fields "x-relevance-evaluation-period" of it)"""
+
+
+__version__ = "0.1.1"
+verbose = 0
+bes_conn = None
+invoke_folder = None
+sites_no_permissions = []
+
+
+def get_invoke_folder(verbose=0):
+    """Get the folder the script was invoked from."""
+    # using logging here won't actually log it to the file:
+
+    if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+        if verbose:
+            print("running in a PyInstaller bundle")
+        invoke_folder = os.path.abspath(os.path.dirname(sys.executable))
+    else:
+        if verbose:
+            print("running in a normal Python process")
+        invoke_folder = os.path.abspath(os.path.dirname(__file__))
+
+    if verbose:
+        print(f"invoke_folder = {invoke_folder}")
+
+    return invoke_folder
+
+
+def get_invoke_file_name(verbose=0):
+    """Get the filename the script was invoked from."""
+    # using logging here won't actually log it to the file:
+
+    if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+        if verbose:
+            print("running in a PyInstaller bundle")
+        invoke_file_path = sys.executable
+    else:
+        if verbose:
+            print("running in a normal Python process")
+        invoke_file_path = __file__
+
+    if verbose:
+        print(f"invoke_file_path = {invoke_file_path}")
+
+    # get just the file name, return without file extension:
+    return os.path.splitext(ntpath.basename(invoke_file_path))[0]
 
 
 def fixlet_xml_add_mime(
@@ -29,7 +81,7 @@ def fixlet_xml_add_mime(
 
     # need to check if mime field already exists in case session relevance is behind
     if mime_field_name in str(fixlet_xml).lower():
-        print("INFO: skipping item, it already has mime field")
+        logging.warning("Skipping item, it already has mime field")
         return None
 
     root_xml = lxml.etree.fromstring(fixlet_xml)
@@ -116,9 +168,10 @@ def put_updated_xml(
         )
         return update_result
     except PermissionError as exc:
-        print(
-            f"ERROR: PermissionError updating fixlet {fixlet_site_name}/{fixlet_id}:{exc}"
+        logging.error(
+            "PermissionError updating fixlet %s/%d:%s", fixlet_site_name, fixlet_id, exc
         )
+        sites_no_permissions.append(fixlet_site_name)
 
     return None
 
@@ -126,20 +179,58 @@ def put_updated_xml(
 def main():
     """Execution starts here."""
     print("main()")
-    bes_conn = besapi.besapi.get_bes_conn_using_config_file()
-    bes_conn.login()
+
+    parser = besapi.plugin_utilities.setup_plugin_argparse()
+
+    # allow unknown args to be parsed instead of throwing an error:
+    args, _unknown = parser.parse_known_args()
+
+    # allow set global scoped vars
+    global bes_conn, verbose, invoke_folder
+    verbose = args.verbose
+
+    # get folder the script was invoked from:
+    invoke_folder = get_invoke_folder(verbose)
+
+    log_file_path = os.path.join(invoke_folder, get_invoke_file_name(verbose) + ".log")
+
+    logging_config = besapi.plugin_utilities.get_plugin_logging_config(
+        log_file_path, verbose, args.console
+    )
+
+    logging.basicConfig(**logging_config)
+
+    logging.info("---------- Starting New Session -----------")
+    logging.debug("invoke folder: %s", invoke_folder)
+    logging.debug("%s's version: %s", get_invoke_file_name(verbose), __version__)
+    logging.debug("BESAPI Module version: %s", besapi.besapi.__version__)
+    logging.debug("Python version: %s", platform.sys.version)
+    logging.warning(
+        "Might get permissions error if not run as a MO or an account with write access to all affected custom content"
+    )
+
+    bes_conn = besapi.plugin_utilities.get_besapi_connection(args)
 
     results = bes_conn.session_relevance_json_array(
         "(id of it, name of site of it) of " + session_relevance_multiple_fixlets
     )
 
-    print(results)
+    logging.debug(results)
 
     for result in results:
         fixlet_id = result[0]
         fixlet_site_name = result[1]
+        fixlet_site_name_safe = urllib.parse.quote(fixlet_site_name, safe="")
 
-        print(fixlet_id, fixlet_site_name)
+        if fixlet_site_name_safe in sites_no_permissions:
+            logging.warning(
+                "Skipping item %d, no permissions to update content in site '%s'",
+                fixlet_id,
+                fixlet_site_name,
+            )
+            continue
+
+        logging.debug(fixlet_id, fixlet_site_name)
 
         fixlet_content = get_content_restresult(bes_conn, fixlet_site_name, fixlet_id)
 
@@ -158,7 +249,9 @@ def main():
         )
 
         if update_result is not None:
-            print(f"Updated fixlet {fixlet_id} in site {fixlet_site_name}:")
+            logging.info("Updated fixlet %d in site %s", fixlet_id, fixlet_site_name)
+
+    logging.info("---------- Ending Session -----------")
 
 
 if __name__ == "__main__":
