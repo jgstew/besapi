6601f5d1d9d236e1c1fb99012d54f4c91e9e542b
JGStew <james@jgstew.com>
2025-09-17 08:56:55 -0400

    reorder tests, improve tests

--- tests/test_besapi.py
+++ tests/test_besapi.py
@@ -231,6 +231,61 @@ def test_bescli():
         assert bes_conn.login()
 
 
+def test_plugin_utilities_win_get_besconn_root_windows_registry():
+    """Test getting a BESConnection from the Windows Registry."""
+    if "ROOT_SERVER" not in os.environ:
+        pytest.skip("Skipping Windows Registry test, ROOT_SERVER not set.")
+    if "ROOT_USER" not in os.environ:
+        pytest.skip("Skipping Windows Registry test, ROOT_USER not set.")
+    if "ROOT_USER_PASSWORD" not in os.environ:
+        pytest.skip("Skipping Windows Registry test, ROOT_USER_PASSWORD not set.")
+
+    if not os.name == "nt":
+        pytest.skip("Skipping Windows Registry test on non-Windows system.")
+
+    # # only run this test if besapi > v3.9.1:
+    # if besapi.besapi.__version__ <= "3.9.1":
+    #     pytest.skip("Skipping test for besapi <= 3.9.1")
+
+    # get env vars for testing:
+    root_server = os.getenv("ROOT_SERVER")
+    root_user = os.getenv("ROOT_USER")
+    root_user_password = os.getenv("ROOT_USER_PASSWORD")
+
+    encrypted_str = besapi.plugin_utilities_win.win_dpapi_encrypt_str(
+        root_user_password
+    )
+
+    import winreg
+
+    # write user and encrypted password to registry for testing:
+    # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\BigFix\Enterprise Server\MFSConfig
+    subkey = r"SOFTWARE\Wow6432Node\BigFix\Enterprise Server\MFSConfig"
+    hive = winreg.HKEY_LOCAL_MACHINE  # type: ignore[attr-defined]
+
+    key = winreg.CreateKey(hive, subkey)
+    winreg.SetValueEx(
+        key, "RESTURL", 0, winreg.REG_SZ, "https://" + root_server + ":52311/api"
+    )
+    winreg.SetValueEx(key, "RESTUsername", 0, winreg.REG_SZ, root_user)
+    winreg.SetValueEx(key, "RESTPassword", 0, winreg.REG_SZ, "{obf}" + encrypted_str)
+    winreg.CloseKey(key)
+    bes_conn = besapi.plugin_utilities_win.get_besconn_root_windows_registry()
+    assert bes_conn is not None
+
+    import bescli
+
+    bigfix_cli = bescli.bescli.BESCLInterface()
+
+    bigfix_cli.BES_ROOT_SERVER = root_server
+    bigfix_cli.BES_USER_NAME = root_user
+    bigfix_cli.BES_PASSWORD = root_user_password
+
+    bigfix_cli.do_saveconf()
+
+    assert bigfix_cli.bes_conn is not None
+
+
 def test_bes_conn_json():
     """Test the BESConnection class with JSON output."""
     bes_conn = besapi.besapi.get_bes_conn_using_config_file()
@@ -273,6 +328,8 @@ def test_bes_conn_json():
         if "10.0." in bes_conn.rootserver:
             print("doing check specific to my env")
             assert "BIGFIX" in result
+    else:
+        pytest.skip("Skipping BESConnection test, no config file or login failed.")
 
 
 def test_plugin_utilities_logging():
@@ -365,46 +422,3 @@ def test_plugin_utilities_win_get_win_registry_rest_pass():
     assert result is not None
     print("Windows Registry REST password:", result)
     assert result == test_string
-
-
-def test_plugin_utilities_win_get_besconn_root_windows_registry():
-    """Test getting a BESConnection from the Windows Registry."""
-    if "ROOT_SERVER" not in os.environ:
-        pytest.skip("Skipping Windows Registry test, ROOT_SERVER not set.")
-    if "ROOT_USER" not in os.environ:
-        pytest.skip("Skipping Windows Registry test, ROOT_USER not set.")
-    if "ROOT_USER_PASSWORD" not in os.environ:
-        pytest.skip("Skipping Windows Registry test, ROOT_USER_PASSWORD not set.")
-
-    if not os.name == "nt":
-        pytest.skip("Skipping Windows Registry test on non-Windows system.")
-
-    # # only run this test if besapi > v3.9.1:
-    # if besapi.besapi.__version__ <= "3.9.1":
-    #     pytest.skip("Skipping test for besapi <= 3.9.1")
-
-    # get env vars for testing:
-    root_server = os.getenv("ROOT_SERVER")
-    root_user = os.getenv("ROOT_USER")
-    root_user_password = os.getenv("ROOT_USER_PASSWORD")
-
-    encrypted_str = besapi.plugin_utilities_win.win_dpapi_encrypt_str(
-        root_user_password
-    )
-
-    import winreg
-
-    # write user and encrypted password to registry for testing:
-    # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\BigFix\Enterprise Server\MFSConfig
-    subkey = r"SOFTWARE\Wow6432Node\BigFix\Enterprise Server\MFSConfig"
-    hive = winreg.HKEY_LOCAL_MACHINE  # type: ignore[attr-defined]
-
-    key = winreg.CreateKey(hive, subkey)
-    winreg.SetValueEx(
-        key, "RESTURL", 0, winreg.REG_SZ, "https://" + root_server + ":52311/api"
-    )
-    winreg.SetValueEx(key, "RESTUsername", 0, winreg.REG_SZ, root_user)
-    winreg.SetValueEx(key, "RESTPassword", 0, winreg.REG_SZ, "{obf}" + encrypted_str)
-    winreg.CloseKey(key)
-    bes_conn = besapi.plugin_utilities_win.get_besconn_root_windows_registry()
-    assert bes_conn is not None
